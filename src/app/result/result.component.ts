99e75e07729ef1f2992c31c88e5e31479c143f17
rgbbot <a@a.com>
2018-05-03 19:44:12 +0300

    result display component

--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -1,12 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-result',
   templateUrl: './result.component.html',
-  styleUrls: ['./result.component.css']
+  styleUrls: ['./result.component.sass']
 })
 export class ResultComponent implements OnInit {
+  @Input() data: any;
+  public chosenCountry: any;
 
+  selectCountry(countryName) {
+    this.chosenCountry = this.data.find(_ => _.countryName === countryName);
+  }
   constructor() { }
 
   ngOnInit() {
